a94c9b5c54e453d00a1ae67d34ade37e9f9dc503
sunny <sunny@lianj.com>
2017-09-04 17:47:18 +0800

    cart.html动画效果 v-for add :key

--- src/pages/cart/cart.js
+++ src/pages/cart/cart.js
@@ -4,18 +4,19 @@ import './cart_base.css'
 
 import url from 'js/api.js'
 import Vue from 'vue'
+import Velocity from 'velocity-animate'
 import axios from 'axios'
 import mixin from 'js/mixin.js'
 import 'mint-ui/lib/style.css'
-import { MessageBox } from 'mint-ui';
+import {MessageBox} from 'mint-ui';
 
 new Vue({
   el: '#app-cart',
   data(){
     return {
-      cartDetail: null,
+      cartList: null,
       isShow: false,
-      goodsChecked:false,
+      goodsChecked: false,
       editingShop: null,  //正在编辑的商铺
       editingShopIndex: -1,
       totalMoney: 0,
@@ -24,6 +25,70 @@ new Vue({
   },
   created(){
     this.getCartDetail()
+  },
+  computed: {
+    allChecked: {
+      get() {
+        if (this.cartList && this.cartList.length) {
+          return this.cartList.every(list => {
+            return list.checked
+          })
+        }
+        return false
+      },
+      set(newVal) {
+        this.cartList.forEach(list => {
+          list.checked = newVal
+          list.goodsList.forEach(goods => {
+            goods.checked = newVal
+          })
+        })
+      }
+    },
+    selectLists() {
+      if (this.cartList && this.cartList.length) {
+        let arr = []
+        let total = 0
+        this.cartList.forEach(list => {
+          list.goodsList.forEach(goods => {
+            if (goods.checked) {
+              arr.push(goods)
+              total += goods.price * goods.number
+            }
+          })
+        })
+        this.totalMoney = total
+        return arr
+      }
+      return []
+    },
+    allRemoteChecked: {
+      get() {
+        if (this.editingShop) {
+          return this.editingShop.goodsList.every(goods => {
+            return goods.removeChecked
+          })
+        }
+        return false
+      },
+      set(newVal) {
+        this.editingShop.goodsList.forEach(goods => {
+          goods.removeChecked = newVal
+        })
+      }
+    },
+    removeLists() {
+      if (this.editingShop) {
+        let arr = []
+        this.editingShop.goodsList.forEach(goods => {
+          if (goods.removeChecked) {
+            arr.push(goods)
+          }
+        })
+        return arr
+      }
+      return []
+    }
   },
   methods: {
     getCartDetail(){
@@ -40,11 +105,11 @@ new Vue({
             goods.removeChecked = false
           })
         })
-        this.cartDetail = lists
+        this.cartList = lists
       })
     },
     // 商品选择
-    chooseGoods(shop,goods){
+    chooseGoods(shop, goods){
       let attr = this.editingShop ? 'removeChecked' : 'checked'
       goods[attr] = !goods[attr];
       shop[attr] = shop.goodsList.every(goods => {
@@ -60,12 +125,12 @@ new Vue({
       })
     },
     // 编辑商铺
-    editing(shop,shopIndex) {
+    editing(shop, shopIndex) {
       shop.editing = !shop.editing
       shop.editingMsg = shop.editing ? '完成' : '编辑'
-      this.cartDetail.forEach((list,i) => {
-        if(shopIndex !== i){
-          list.editing= false
+      this.cartList.forEach((list, i) => {
+        if (shopIndex !== i) {
+          list.editing = false
           list.editingMsg = shop.editing ? '' : '编辑'
         }
       })
@@ -79,45 +144,43 @@ new Vue({
     },
     // 添加商品
     add(goods) {
-      axios.post(url.addNum,{
+      axios.post(url.addNum, {
         id: goods.id,
         number: 1
       }).then((res) => {
-        if(res.status === 200) {
+        if (res.status === 200) {
           goods.number++
         }
       })
     },
     // 减少商品
     reduce(goods){
-      if(goods.number === 1) return
-      axios.post(url.reduceNum,{
+      if (goods.number === 1) return
+      axios.post(url.reduceNum, {
         id: goods.id,
         number: 1
       }).then((res) => {
-        if(res.status === 200) {
+        if (res.status === 200) {
           goods.number--
         }
       })
     },
     // 删除单个商品
-    remove(shop,shopIndex,goods,goodsIndex) {
-      if(this.editingShop) {
-        this.removeData = {shop,shopIndex,goods,goodsIndex}
-        MessageBox.confirm('确定要删除当前商品吗？').then(() => {
-          axios.post(url.remove,{id: goods.id}).then((res) => {
-            if(res.status === 200){
-              shop.goodsList.splice(goodsIndex,1)
-              if(!shop.goodsList.length) {
-                this.cartDetail.splice(shopIndex,1)
-                this.removeShop()
-              }
+    remove(shop, shopIndex, goods, goodsIndex) {
+      this.removeData = {shop, shopIndex, goods, goodsIndex}
+      MessageBox.confirm('确定要删除当前商品吗？').then(() => {
+        axios.post(url.remove, {id: goods.id}).then((res) => {
+          if (res.status === 200) {
+            shop.goodsList.splice(goodsIndex, 1)
+            if (!shop.goodsList.length) {
+              this.cartList.splice(shopIndex, 1)
+              this.removeShop()
             }
-          }) 
-        },() => {
-          this.removeShop()
+          }
         })
-      }
+      }, () => {
+        this.removeShop()
+      })
       // console.log(MessageBox)
     },
     // 删除多个商品
@@ -127,102 +190,57 @@ new Vue({
         this.removeLists.forEach(goods => {
           ids.push(goods.id)
         })
-        axios.post(url.removeMore,ids).then((res) => {
-          if(res.status === 200) {
+        axios.post(url.removeMore, ids).then((res) => {
+          if (res.status === 200) {
             let arr = []
             this.editingShop.goodsList.forEach(goods => {
               // 找到被选中删除的索引
               let index = this.removeLists.findIndex(remove => {
-                return  remove.id === goods.id
+                return remove.id === goods.id
               })
-              if(index === -1) {
+              if (index === -1) {
                 arr.push(goods)
               }
             })
-            if(arr.length) {
+            if (arr.length) {
               this.editingShop.goodsList = arr
-            }else {
-              this.cartDetail.splice(this.editingShopIndex,1)
+            } else {
+              this.cartList.splice(this.editingShopIndex, 1)
               this.removeShop()
             }
           }
         })
-       },() =>{
-         this.removeShop()
-       })
+      }, () => {
+        this.removeShop()
+      })
     },
     // 删除商品后回到编辑状态
-    removeShop(shop) {
+    removeShop() {
       this.editingShop = null;
       this.shopIndex = -1;
-      this.cartDetail.forEach(shop => {
+      this.cartList.forEach(shop => {
         shop.editing = false  // 把编辑状态设置为false
         shop.editingMsg = '编辑'
       })
-    }
-  },
-  computed: {
-    allChecked: {
-      get() {
-        if(this.cartDetail && this.cartDetail.length){
-            return this.cartDetail.every(list => {
-              return list.checked
-            })
-        }
-        return false
-      },
-      set(newVal) {
-        this.cartDetail.forEach(list => {
-          list.checked = newVal
-          list.goodsList.forEach(goods => {
-            goods.checked = newVal
-          })
-        })
-      }
     },
-    selectLists() {
-      if(this.cartDetail && this.cartDetail.length) {
-        let arr = []
-        let total = 0
-        this.cartDetail.forEach(list => {
-          list.goodsList.forEach(goods => {
-            if(goods.checked) {
-              arr.push(goods)
-              total+= goods.price  * goods.number
-            }
-          })
-        })
-        this.totalMoney = total
-        return arr
-      }
-      return []
+    // 移动效果
+    start(e, goods){
+      goods.startX = e.changedTouches[0].clientX //获取手指的开始触发距最右边的距离
     },
-    allRemoteChecked: {
-      get() {
-        if(this.editingShop) {
-          return this.editingShop.goodsList.every(goods => {
-            return goods.removeChecked
-          })
-        }
-        return false
-      },
-      set(newVal) {
-        this.editingShop.goodsList.forEach(goods => {
-          goods.removeChecked = newVal
-        })
+    end(e, shopIndex, goods, goodsIndex) {
+      let endX = e.changedTouches[0].clientX; // 手指结束触发距离最右边的距离
+      let left = '0';
+      if (goods.startX - endX > 60) {
+        left = '-60px'
       }
-    },
-    removeLists() {
-      if(this.editingShop) {
-        let arr = []
-        this.editingShop.goodsList.forEach(goods => {
-          if(goods.removeChecked) {
-            arr.push(goods)
-          }
-        })
-        return arr
+      if (endX - goods.startX > 60) {
+        left = '0px'
       }
-      return []
+      // console.log(this.$refs)
+      // console.log(e)
+      Velocity(this.$refs[`goods-${shopIndex}-${goodsIndex}`], {
+        left
+      })
     }
   },
   mixins: [mixin]
